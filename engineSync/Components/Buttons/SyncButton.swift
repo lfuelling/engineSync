9e005d4c2855ba386c514251a01d54110b11fc32
Lukas Fülling <lukas@k40s.net>
2022-11-20 03:24:54 +0100

    start work on sync

--- engineSync/Components/Buttons/SyncButton.swift
+++ engineSync/Components/Buttons/SyncButton.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct SyncButton: View {
     @EnvironmentObject var appState: AppState
 
+    @State private var showError = false
+
     var body: some View {
         VStack {
             if appState.loading {
@@ -13,10 +15,31 @@ struct SyncButton: View {
                 }
             } else {
                 Button(action: {
-                    appState.loading = true
-                }, label: {Text("Sync").frame(maxWidth: .infinity)}).disabled(syncButtonDisabled())
+                    Task {
+                        appState.loading = true
+                        appState.statusText = "Creating target directories..."
+
+                        do {
+                            try FileManager.default.createDirectory(atPath: "\(appState.targetDrivePath)/Engine Library/Database2", withIntermediateDirectories: true, attributes: nil)
+                            try FileManager.default.createDirectory(atPath: "\(appState.targetDrivePath)/SoundSwitch", withIntermediateDirectories: true, attributes: nil)
+                        } catch {
+                            showError = true
+                            print(error)
+                        }
+
+                        appState.statusText = "Finished!"
+                        appState.loading = false
+                    }
+                }, label: { Text("Sync").frame(maxWidth: .infinity) })
+                        .disabled(syncButtonDisabled())
+                        .alert("An error occurred!", isPresented: $showError) {
+                            Button("OK", role: .cancel) {
+                                showError = false
+                            }
+                        }
             }
-        }.padding()
+        }
+                .padding()
     }
 
     func syncButtonDisabled() -> Bool {
